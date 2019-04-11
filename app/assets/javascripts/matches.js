15eaf11a0bb50211df2d1e3a7aea028e4626730c
das3in <reyes.carlj@gmail.com>
2019-04-11 13:55:58 -0400

    Added places for predictions

--- app/assets/javascripts/matches.js
+++ app/assets/javascripts/matches.js
@@ -1,19 +1,21 @@
 const load_teams_from_division = () => {
   const tournamentId = $('#tournamentId').val();
   let division = $('#division-select').val();
-  const request = `/tournaments/${tournamentId}/teams_for_division?division=${division}`
+  const request = `/tournaments/${tournamentId}/teams_for_division?division=${division}`;
 
   let aj = $.ajax({
     url: request,
     type: 'get',
-    data: $(this).serialize()
-  }).done((data) => {
-    change_teams(data)
-  }).fail(err => {
-    console.log('Something messed up badly');
-    console.log(err)
+    data: $(this).serialize(),
   })
-}
+    .done(data => {
+      change_teams(data);
+    })
+    .fail(err => {
+      console.log('Something messed up badly');
+      console.log(err);
+    });
+};
 
 const change_teams = data => {
   $('#match_home_team_id').empty();
@@ -22,16 +24,22 @@ const change_teams = data => {
 
   data.map(team => {
     $('#match_home_team_id').append(
-      $('<option></option>').attr('value', team.id).text(team.team.name)
-    )
+      $('<option></option>')
+        .attr('value', team.id)
+        .text(team.team.name),
+    );
     $('#match_away_team_id').append(
-      $('<option></option>').attr('value', team.id).text(team.team.name)
-    )
+      $('<option></option>')
+        .attr('value', team.id)
+        .text(team.team.name),
+    );
     $('#match_winner_id').append(
-      $('<option></option>').attr('value', team.id).text(team.team.name)
-    )
-  })
-}
+      $('<option></option>')
+        .attr('value', team.id)
+        .text(team.team.name),
+    );
+  });
+};
 
 $(document).on('change', '#division-select', () => {
   load_teams_from_division();
